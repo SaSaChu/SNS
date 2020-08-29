def4717165a2c8bec7e955f9c9420283f12647f0
SaSaChu <teresa7397@gmail.com>
2020-08-29 19:14:19 +0800

    ‘第一款內頁ok’

--- js/public.js
+++ js/public.js
@@ -1,9 +1,42 @@
-/**
- * @author      OA Wu <comdan66@gmail.com>
- * @copyright   Copyright (c) 2015 - 2019, Ginkgo
- * @license     http://opensource.org/licenses/MIT  MIT License
- * @link        https://www.ioa.tw/
- */
+
  
 $(function() {
+  
+    // 手機選單
+  $('.m_Hamburg').click(function() {
+    $('.menu_boxs').addClass ('hovermenu');
+    $('.cover').addClass('show');
+    scrollLock();
+  });
+  $('.menu_boxs .icon-x').click(function() {
+    $('.menu_boxs').removeClass ('hovermenu');
+    $('.cover').removeClass('show');
+    scrollUnlock();
+  });
+
+  $('.threeMenu').click(function() {
+    // $('.lastMenu_boxs').toggleClass ('showMenu');
+    $(this).next().toggleClass ('showMenu');
+  });
+
+  // // 手機選單模式，背景資訊鎖定
+  function scrollLock () {
+    var scrollPosition = [
+      self.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft,
+      self.pageYOffset || document.documentElement.scrollTop  || document.body.scrollTop
+    ];
+    var $html = $('html, body'); // it would make more sense to apply this to body, but IE7 won't have that
+    $html.data('scroll-position', scrollPosition);
+    $html.data('previous-overflow', $html.css('overflow'));
+    $html.css('overflow', 'hidden');
+    window.scrollTo(scrollPosition[0], scrollPosition[1]);
+  }
+
+  function scrollUnlock () {
+    var $html = $('html, body'); // it would make more sense to apply this to body, but IE7 won't have that
+    var scrollPosition = $html.data('scroll-position');
+    $html.css('overflow', $html.data('previous-overflow'));
+    window.scrollTo(scrollPosition[0], scrollPosition[1]);
+  }
+
 });
